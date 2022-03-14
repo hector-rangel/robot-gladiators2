768d70b039beca96d5629b9edbe1e70d28be046a
Hector Rangel <hector.rangeljr@yahoo.com>
2022-03-14 10:49:42 -0500

    added game.js

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+console.log(10 + 10);
+console.log("our robots name is" + playerName);
+
+var name = "your name";
+console.log(name);
+
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+
+// fight();
